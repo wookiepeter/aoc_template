36d54ef9fecc89a0b01e196674005d36bb32f64b
Matthis Hagen <matthis.hagen@o2online.de>
2023-11-30 18:18:10 +0100

    Initial commit

---
+++ src/bin/part1.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let input = include_str!("./input1.txt");
+    let output = process(input);
+    dbg!(output);
+}
+
+fn process(input: &str) -> String {
+    input.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::process;
+
+    #[test]
+    fn test_example() {
+        let result = process("");
+        assert_eq!(result, "4".to_string())
+    }
+}
